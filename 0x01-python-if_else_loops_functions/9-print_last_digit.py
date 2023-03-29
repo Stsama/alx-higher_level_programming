8c98cb79cc4266270df44fdfab962c5fc1ebc95a
Stsama <albertezoula98@gmail.com>
2023-03-29 06:04:33 -0700

    task 8 & 9 reviewed

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # created by Albert Ezoula
 def print_last_digit(number):
-    result = abs(number) % 10
-    if number < 0:
-        result = -reslut
-    return result
+    """Print the last digit of a number and return it."""
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
 
