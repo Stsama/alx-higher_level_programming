e001b1768360d34dbda2c3eb403148a090a2d676
Stsama <albertezoula98@gmail.com>
2023-03-29 07:30:03 -0700

    task 100 correct

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 100-print_tebahpla.py
 # created by  Albert Ezoula
-for i in range(97, 123):
-    if i % 2 == 0:
+for i in reversed(range(97, 123)):
+    if i % 2 != 0:
         i -= 32
-    print("{}".format(chr(i)))
+    print("{}".format(chr(i)), end="")
