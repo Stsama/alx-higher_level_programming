8ec91432b7fad43fb2599008ca7c3a878cad29a3
Stsama <albertezoula98@gmail.com>
2023-03-29 06:16:55 -0700

    task 10 & 11 solved

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# 10-add.py
+# created by Albert Ezoula
+def add(a, b):
+    return a + b
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# 11-pow.py
+# created by albert Ezoula
+def pow(a, b):
+    return a**b
