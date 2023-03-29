0e7af3419f45c46dbfc40c83cc5a138b231fc00f
Stsama <albertezoula98@gmail.com>
2023-03-29 06:31:33 -0700

    task 12 review

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,8 +4,8 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
-            print("Fizz ")
+            print("Fizz ", end="")
         elif i % 5 == 0:
-            print("Buzz ")
+            print("Buzz ", end="")
         else:
-            print("{} ".format(i))
+            print("{} ".format(i), end="")
