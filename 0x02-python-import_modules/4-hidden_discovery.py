0d046b750c83b778fcaf0a78f8cb46404325de1b
Stsama <albertezoula98@gmail.com>
2023-04-01 15:10:33 -0700

    task 4 corrected

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,9 @@
 # Albert Ezoula
 if __name__ == "__main__":
     """prints all the names defined by the compiled module hidden_4.pyc"""
-    
+
     import hidden_4
+
     names = dir(hidden_4)
     for name in names:
         if name[:2] != "__":
