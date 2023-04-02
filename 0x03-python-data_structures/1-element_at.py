1160ebcbb633c4c39cc891ffe3942e3bae75493e
Stsama <albertezoula98@gmail.com>
2023-04-02 12:31:05 -0700

    task 1 fixed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,5 +4,5 @@
 def element_at(my_list, idx):
     """retrieves an element from a list like in C"""
     if idx < 0 or idx >= len(my_list):
-        return ""
-    return my_list.index(idx)
+        return None
+    return my_list[idx]
