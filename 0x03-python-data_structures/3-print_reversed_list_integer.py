3d0a730f59d8ddb5acadcd1bbd1e5cdd58b94879
Stsama <albertezoula98@gmail.com>
2023-04-02 12:50:25 -0700

    task 3 solved

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/puthon3
+# 3-print_reversed_list_integer.py
+# Albert Ezoula
+def print_reversed_list_integer(my_list=[]):
+    """prints all integers of a list, in reverse order"""
+    for i in reversed(len(my_list) - 1):
+        print("{}".format(i))
