afe5eecec6cdddf544df0f38edbbf9623a6e69f2
Stsama <albertezoula98@gmail.com>
2023-04-13 15:36:38 -0700

    task 8 done

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 8-simple_delete.py
+# Albert Ezoula
+def simple_delete(a_dictionary, key=""):
+    """deletes a key in a dictionary."""
+    del a_dictionary[key]
+    return (a_dictionay)
