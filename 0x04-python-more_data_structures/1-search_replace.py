ba4772592a5e9e0077f58f6dd4b820dd912bf5bc
Stsama <albertezoula98@gmail.com>
2023-04-13 14:35:32 -0700

    task 1 corrected

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 # Albert Ezoula
 def search_replace(my_list, search, replace):
     """replaces all occurrences of an element by another in a new list"""
-    new_list[] = my_list[:]
+    new_list = my_list[:]
     for item in range(len(new_list)):
         if new_list[item] == search:
             new_list[item] = replace
