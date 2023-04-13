b50d99d1091e73923b9db3fc619bf22e717cc26e
Stsama <albertezoula98@gmail.com>
2023-04-13 15:27:25 -0700

    task 7 done

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 7-update_dictionary.py
+# Albert Ezoula
+def update_dictionary(a_dictionary, key, value):
+    """replaces or adds key/value in a dictionary."""
+
+    a_dictionary[key] = value
+    return (a_dictionary)
