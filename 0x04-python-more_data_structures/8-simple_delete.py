8e435e9367aae65ee59676ec75b8f29cb5261c37
Stsama <albertezoula98@gmail.com>
2023-04-13 15:39:20 -0700

    task 8 fixed

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,5 +3,6 @@
 # Albert Ezoula
 def simple_delete(a_dictionary, key=""):
     """deletes a key in a dictionary."""
-    del a_dictionary[key]
+    if key in a_dictionary:
+        del a_dictionary[key]
     return (a_dictionay)
