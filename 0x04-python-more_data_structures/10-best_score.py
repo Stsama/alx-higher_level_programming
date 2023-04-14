49518bd5ac3a713663b259a771199492e6b8c2b9
Stsama <albertezoula98@gmail.com>
2023-04-14 12:46:09 -0700

    task 10 bug fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,7 +7,7 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    first = list(a_dictionary.keys())[0]i
+    first = list(a_dictionary.keys())[0]
     big = a_dictionary[first]
 
     for key, value in a_dictionary.items():
