82f5de339eb2614bbc27e3c582a5bdd14a29fc33
Stsama <albertezoula98@gmail.com>
2023-04-14 06:47:06 -0700

    task 9 reviewed

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@
 # Albert Ezoula
 def multiply_by_2(a_dictionary):
     """returns a new dictionary with all values multiplied by 2"""
-    return ({k: a_ditionary[k] * 2 for k in a_dictionary})
+    return ({k: a_ditionary[k] * 2 for k in sorted(a_dictionary)})
