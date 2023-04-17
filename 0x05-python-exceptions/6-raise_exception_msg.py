56c88b920cc3560ff64707cdcbc59e3d1561e947
Stsama <albertezoula98@gmail.com>
2023-04-17 14:15:10 -0700

    task 6 ended

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Albert Ezoula
+def raise_exception_msg(message=""):
+    """raises a name exception with a message."""
+    raise NameError(message)
