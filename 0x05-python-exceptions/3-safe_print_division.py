9832b59720ea2bec85c241485177100314c4e7ed
Stsama <albertezoula98@gmail.com>
2023-04-17 13:19:11 -0700

    task 3 fixed

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -9,5 +9,5 @@ def safe_print_division(a, b):
     except (TypeError, ZeroDivisionError):
         result = None
     finally:
-        print("Inside result: {}".format(div))
-    return (div)
+        print("Inside result: {}".format(result))
+    return (result)
