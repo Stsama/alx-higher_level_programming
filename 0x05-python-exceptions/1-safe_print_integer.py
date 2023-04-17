42e1889610299772fdf2d610bd7da7dd89a54ad4
Stsama <albertezoula98@gmail.com>
2023-04-17 12:42:39 -0700

    task 1 done

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-safe_print_integer.py
+# Albert Ezoula
+def safe_print_integer(value):
+    """prints an integer with "{:d}".format()."""
+
+    try:
+        print("{}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
