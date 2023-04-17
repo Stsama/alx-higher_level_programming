25e71590d6222fcbfbdf346e38becb759fd66291
Stsama <albertezoula98@gmail.com>
2023-04-17 13:16:46 -0700

    task 3 done

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 3-safe_print_division.py
+# Albert Ezoula
+def safe_print_division(a, b):
+    """divides 2 integers and prints the result."""
+
+    try:
+        result = a / b
+    except (TypeError, ZeroDivisionError):
+        result = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
