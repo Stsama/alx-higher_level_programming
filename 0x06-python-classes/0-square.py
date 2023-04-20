4de77f3827d306de31340f9b2aeef3fd933c1273
Stsama <albertezoula98@gmail.com>
2023-04-20 13:03:33 -0700

    task 0 done

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 0-square.py
+# Albert Ezoula
+class Square:
+    """an empty class Square that defines a square"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+this is the readme of 0x06-python-classes0x
