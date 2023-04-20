516a529b94f77e192e2a6a0787cdcb45e914b59d
Stsama <albertezoula98@gmail.com>
2023-04-20 13:26:20 -0700

    task 1 try

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 1-square.py
+# Albert Ezoula
+"""class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """Reprersent a class"""
+
+    def __init__(self, size):
+        """initialize a new square.
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
