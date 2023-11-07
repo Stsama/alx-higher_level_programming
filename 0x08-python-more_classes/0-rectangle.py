ec8c25e199da6cb4e96d6bc30853448cb178b011
Stsama <albertezoua98@gmail.com>
2023-11-07 11:43:50 -0800

    task 1 of 0x08 done

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # 0-rectangle.py
-#Created by Albert Ezoula
+# Created by Albert Ezoula
 """Define a Rectengle class"""
+
+
 class Rectangle:
     """represent a rectengle class"""
+
     pass
