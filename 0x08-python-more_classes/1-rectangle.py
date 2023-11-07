fff96fb2425859af2ecf2f161bd59daab63953b6
Stsama <albertezoua98@gmail.com>
2023-11-07 12:06:09 -0800

    task 1 fixed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,27 +9,18 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initilisation of the square"""
-        self.__width = width
         self.__height = height
-
-    @property
-    def width(self):
-        """retrieves the value of the width"""
-        return (self.__width)
-
-    @width.setter
-    def width(self, value):
-        """sets the value of width"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        self.__width = width
 
     @property
     def height(self):
         """retrieves the value of the height"""
         return (self.__height)
+
+    @property
+    def width(self):
+        """retrieves the value of the width"""
+        return (self.__width)
 
     @height.setter
     def height(self, value):
@@ -39,3 +30,12 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @width.setter
+    def width(self, value):
+        """sets the value of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
