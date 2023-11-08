512c0a20cad82a87ad7c505d7309c5430fab4b89
Stsama <albertezoua98@gmail.com>
2023-11-07 16:30:28 -0800

    task 8 done

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 7-rectangle.py
+# 8-rectangle.py
 # created by Albert Ezoula
 """Define a rectangle class"""
 
@@ -8,7 +8,6 @@ class Rectangle:
     """Represent a rectangle."""
 
     number_of_instances = 0
-    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -67,7 +66,7 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
+            [rect.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
@@ -82,3 +81,21 @@ class Rectangle:
         """deletes an instance"""
         print("Bye rectangle...""")
         Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """returns the biggest rectangle based on the area.
+
+        Args:
+            rect_1 (rectangle) : The first intance to compare
+            rect_2 (rectangle) : The second intance to compare
+        Raises:
+            TypeError : If one of the rectangle is greater than the other.
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
