95162916aed3a12315463143229071443f10e5a5
Stsama <albertezoua98@gmail.com>
2023-11-08 17:12:08 -0800

    task 1

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,9 +4,6 @@
 """module that difine a class"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
 class Rectangle(BaseGeometry):
     """Represent a rectangle using BaseGeometry."""
 
