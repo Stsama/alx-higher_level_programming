ec638965ff6171424e25383f4f348e52b0ab43f5
Stsama <albertezoua98@gmail.com>
2023-11-08 17:06:44 -0800

    tas 1-10 doner

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 10-square.py
+# Created by Albert Ezoula
+"""module that difine a class"""
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
