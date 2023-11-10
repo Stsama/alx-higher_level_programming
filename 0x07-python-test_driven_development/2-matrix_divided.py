72cdea3a7588eba1ad8547e09c463cf78fc0d399
Stsama <albertezoua98@gmail.com>
2023-11-10 10:14:48 -0800

    task 0 tested

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 2-matrix_divided.py
+# Albert Ezoula
+def matrix_divided(matrix, div):
+    """function that divides all elements of a matrix."""
+    for i in range(matrix):
+        
