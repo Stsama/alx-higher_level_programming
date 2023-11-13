7f85c4fb358b794ad0ad22be0cc7091628d994c7
Stsama <albertezoua98@gmail.com>
2023-11-13 15:30:25 -0800

    task 0 refactoring

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 # 0-lookup.py
 # Created by Albert Ezoula
-"""define a fonction called lookup"""
-
-
 def lookup(obj):
     """ returns the list of available attributes and methods of an object"""
     return (dir(obj))
