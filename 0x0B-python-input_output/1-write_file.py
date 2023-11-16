0e25ab545a4a4323813108961c9342f6930a56df
Stsama <albertezoua98@gmail.com>
2023-11-16 11:09:15 -0800

    task 1 fixed

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,9 +5,13 @@
 
 
 def write_file(filename="", text=""):
-    """
-    function that appends a string at the end of a text file
-    (UTF8) and returns the number of characters added:
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
