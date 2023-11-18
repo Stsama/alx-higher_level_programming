828124be927212c4e871b9539e3f631e5afbac10
Stsama <albertezoua98@gmail.com>
2023-11-18 14:46:07 -0800

    task 5 done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,9 @@ class Rectangle(Base):
             [print(" ", end="") for x in range(self.x)]
             [print("#", end="") for w in range(self.width)]
             print("")
+
+    def __str__(self):
+        """that it returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        a = f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y} "
+        b = f"- {self.width}/{self.height}"
+        return a + b
