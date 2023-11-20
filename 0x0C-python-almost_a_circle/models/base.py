4415dce297883df75b11a0710187e64c5f380f3e
Stsama <albertezoua98@gmail.com>
2023-11-20 15:22:15 -0800

    task 14done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # models/base.py
 # Albert Ezoula
-"""DDefine a class Base"""
+"""Define a class Base"""
+import json
 
 
 class Base:
@@ -19,3 +20,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
