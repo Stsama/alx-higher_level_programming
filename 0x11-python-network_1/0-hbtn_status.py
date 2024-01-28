a9b4861bec42cebc4162dc166bb0e68cb9477157
Stsama <albertezoua98@gmail.com>
2024-01-28 14:03:36 -0800

    task 0 done

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# fetches https://alx-intranet.hbtn.io
+if __name__ == "__main__":
+    from urllib.request import Request, urlopen
+    req = Request('https://alx-intranet.hbtn.io/status')
+    with urlopen(req) as response:
+        html = response.read()
+        print('Body response')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+learning how to use urllib and requests module in python
