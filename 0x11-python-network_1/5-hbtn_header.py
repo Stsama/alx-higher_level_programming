30dd113732f55ad5eff7fa09e0fc68e069f98e1a
Stsama <albertezoua98@gmail.com>
2024-01-28 18:02:28 -0800

    altask 5 done

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""sends a request to the URL and displays the value of the variable """
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    req = requests.get(url)
+    print(req.headers.get('X-Request-Id'))
