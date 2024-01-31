af05929d24feaed3943d330cb669dc17b073bf5b
Stsama <albertezoua98@gmail.com>
2024-01-31 00:58:39 -0800

    task 100 fixed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     try:
         for i in range(10):
             print("{}: {}".format(
-                commit[i].get("sha"),
-                commit[i].get("author").get("name")))
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
