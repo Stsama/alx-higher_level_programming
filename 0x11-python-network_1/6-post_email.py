4cbca4c5a195fc770bc67a7f0ee151c1190f4c35
Stsama <albertezoua98@gmail.com>
2024-01-30 20:48:51 -0800

    task 6 done

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" sends a POST request to the passed URL with the email as a parameter"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+    email={"email": sys.argv[2]}
+    req = requests.post(sys.argv[1], data=email)
+    print(req.text)
