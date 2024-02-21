f2726df7d3c162dab0331c4a4cd993d671182323
Stsama <albertezoua98@gmail.com>
2024-02-20 16:21:33 -0800

    task 0 fixed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const fs = require("fs");
-const response = fs.readFile(process.argv[2], "utf-8", function (err, data){
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   console.log(err || data);
 });
